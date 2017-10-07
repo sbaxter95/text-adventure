6292d90b418150469933c7db81db709487f8f7a3
Sophie Baxter <sbaxter95@outlook.com>
2017-10-07 18:54:45 +0100

    Input and Text Managers set up

---
+++ InputManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputManager : MonoBehaviour {
+
+	public Text text;
+	public InputField inputField;
+	public string playerText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerText = inputField.text;
+		if (Input.GetKeyDown(KeyCode.Return)) {
+			text.text += (playerText + '\n'); 
+			inputField.text = "";
+		}
+	}
+}
---
+++ TextManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextManager : MonoBehaviour {
+
+	public Text text;
+
+	// Use this for initialization
+	void Start () {
+		text.text = "Welcome to the game!" + '\n';
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
